4f37a39b654f6176fd036dc18460e680648fa02f
Josef Hare-Brown <josefhb@hotmail.com>
2022-07-12 14:26:41 +0100

    taken away email

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -88,11 +88,6 @@ const Navbar = () => {
                         Github <FaGithub size={30}/>
                     </a>
                 </li>
-                <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#6fc2b0]'>
-                    <a className= 'flex justify-between items-center w-full text-gray-300' href="/">
-                        Email <HiOutlineMail size={30}/>
-                    </a>
-                </li>
                 <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]'>
                     <a className= 'flex justify-between items-center w-full text-gray-300' href="/">
                         CV <BsPersonLinesFill size={30}/>
