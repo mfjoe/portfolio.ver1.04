0a3f40a11a2d68e53f65fe932988953a0e7914da
Josef Hare-Brown <josefhb@hotmail.com>
2022-08-06 17:56:17 +0100

    added cv

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -89,7 +89,7 @@ const Navbar = () => {
                     </a>
                 </li>
                 <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]'>
-                    <a className= 'flex justify-between items-center w-full text-gray-300' href="/">
+                    <a className= 'flex justify-between items-center w-full text-gray-300' href="https://drive.google.com/file/d/1_58dDKZGRI9CoWLRmVVM_7momA6paFBV/view?usp=sharing">
                         CV <BsPersonLinesFill size={30}/>
                     </a>
                 </li>
